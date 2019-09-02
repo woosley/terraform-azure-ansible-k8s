49d2db39535317bc3c1b5f204dd12a4d3e38c49d
woosley.xu <woosley.xu@gmail.com>
2019-09-03 00:30:42 +0800

    update

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "azurerm_virtual_machine" "k8s" {
 
 }
 
-resource "null_resource" "ac" {
+resource "null_resource" "aac" {
   triggers {
     cluster_instance_ids = "${join(",", azurerm_virtual_machine.k8s.*.id)}"
   }
