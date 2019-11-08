5ae62a763b8f7159f8cc5635387f84a14aef4320
woosley.xu <woosley.xu@gmail.com>
2019-11-08 14:37:03 +0800

    fix flannel

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "azurerm_virtual_machine" "k8s" {
 
 }
 
-resource "null_resource" "ec" {
+resource "null_resource" "aec" {
   triggers {
     cluster_instance_ids = "${join(",", azurerm_virtual_machine.k8s.*.id)}"
   }
