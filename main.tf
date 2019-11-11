eacd93fbacbe5cf7c7fd5189d5e234d095a32cac
woosley.xu <woosley.xu@gmail.com>
2019-11-12 00:18:01 +0800

    docker is ready

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "azurerm_virtual_machine" "k8s" {
 
 }
 
-resource "null_resource" "aec" {
+resource "null_resource" "abec" {
   triggers {
     cluster_instance_ids = "${join(",", azurerm_virtual_machine.k8s.*.id)}"
   }
