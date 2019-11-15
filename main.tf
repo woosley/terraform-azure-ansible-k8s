5e261a4e99429d61a7c6d3b25f929a6a8497ab4c
woosley.xu <woosley.xu@gmail.com>
2019-11-15 19:00:47 +0800

    start to work on kubelet

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "azurerm_virtual_machine" "k8s" {
 
 }
 
-resource "null_resource" "abec" {
+resource "null_resource" "bec" {
   triggers {
     cluster_instance_ids = "${join(",", azurerm_virtual_machine.k8s.*.id)}"
   }
